e69760eddc84a6f161ebf57c1ee746f410ec161e
BrettBlox <brettbloxom@gmail.com>
2019-08-24 14:14:34 -0600

    netlify identity

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
+import netlifyIdentity from 'netlify-identity-widget'
+
+import { loginUser, logoutUser } from './identityActions'
+
+import { Button } from 'react-materialize'
+
 import AddEmployee from './components/AddEmployee'
 import EmployeeList from './components/EmployeeList'
 import FilterEmployees from './components/FilterEmployees'
 
-import './styles/App.css'
+export default class App extends Component {
+  state = { user: null }
 
-export default function App() {
-  return (
-    <div className='App card-panel'>
-      <AddEmployee />
-      <FilterEmployees />
-      <EmployeeList />
-    </div>
-  )
+  componentDidMount() {
+    const user = localStorage.getItem('currentUser')
+    if (user) {
+      this.setState({ user: JSON.parse(user) })
+    } else {
+      loginUser()
+    }
+    netlifyIdentity.on('login', user => this.setState({ user }, loginUser()))
+    netlifyIdentity.on('logout', user =>
+      this.setState({ user: null }, logoutUser())
+    )
+  }
+
+  handleLogIn = () => {
+    netlifyIdentity.open()
+  }
+
+  handleLogOut = () => {
+    netlifyIdentity.logout()
+  }
+
+  render() {
+    return (
+      <div className='App card-panel'>
+        {this.state.user ? (
+
+        <AddEmployee />
+        <FilterEmployees />
+        <EmployeeList />
+        ) : (
+          <>
+            <Button
+              large
+              waves-light
+              className='login-button'
+              onClick={this.handleLogIn}>
+              Login
+            </Button>
+          </>
+      </div>
+    )
+  }
 }
